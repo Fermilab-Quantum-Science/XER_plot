15b648e0bba3e656d5014672f6ad17c78b9e2b64
Jim Kowalkowski <jbk@fnal.gov>
2022-10-14 10:54:21 -0500

    working gv and nx

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from hmac import trans_5C
 import graphviz as gv
 import networkx as nx
 from xerparser.reader import Reader
@@ -8,11 +9,11 @@ def make_sample_graph():
     dot.node("A","name1")
     dot.node("B","name2")
     dot.edge("A","B", constraint='false')
-    dot.render('doctest-output/tmp.gv').replace('\\', '/')
-    dot.render('doctest-output/tmp.gv', view=True)
+    dot.render('tmp.gv').replace('\\', '/')
+    dot.render('tmp.gv', view=True)
 
 
-def process_up(first_id,dot, tot, parent_ms=False):
+def gv_go_up(first_id,dot, xer, tot):
     first = xer.activities.find_by_id(first_id)
     succs = xer.relations.get_successors(first.task_id)
 
@@ -23,92 +24,168 @@ def process_up(first_id,dot, tot, parent_ms=False):
         extra = sact.task_name if sact.task_type=='TT_FinMile' else "T"
         if first.task_type == 'TT_FinMile' or True:
             if sact.task_type != 'TT_FinMile' or True:
-                dot.node(str(sact.task_id), f'{sact.task_code}/{extra[0:18]}')
+                dot.node(str(sact.task_id), f'{sact.task_code}/{extra[0:20]}')
 
-            print(f'  up edge id={s.task_id}, pred={s.pred_task_id}')
+            # print(f'  up edge id={s.task_id}, pred={s.pred_task_id}')
             dot.edge(str(s.task_id), str(s.pred_task_id), color='gray')
 
     for s in succs:    
-        process_up(s.task_id,dot,tot+1,parent_ms)
+        gv_go_up(s.task_id,dot,xer, tot+1)
 
+gvdb = {}
 
-def process_node(first_id,dot, tot,parent_ms=False):
+def gv_add_node(first_id, last, dot, xer, tot):
     first = xer.activities.find_by_id(first_id)
-    print(f'adding node {first.task_id}, {first.task_code} / {first.task_name}')
+    #print(f'adding node {first.task_id}, {first.task_code} / {first.task_name}')
+    rc=False
 
-    if first.task_type != 'TT_FinMile':
-        pass # return
+    if first.task_id in gvdb:
+        print(f'{first.task_id} already processed completely')
+        return gvdb[first.task_id]
+
+    if first.task_type == 'TT_FinMile':
+        desc = f'{first.task_code}/{first.task_name[0:]}'
     else:
-        dot.node(str(first.task_id), f'{first.task_code}/{first.task_name[0:]}')
+        desc = f'{first.task_code}'
 
-    if tot>3: return
+    if first.task_id == last.task_id:
+        dot.node(str(first.task_id), desc)
+        print("hit first == last")
+        return True
 
     preds = xer.relations.get_predecessors(first.task_id)
 
+    if len(preds)==0:
+        # print(f"hit end of path {first.task_code}")
+        return False
+
     for p in preds:
-        pact = xer.activities.find_by_id(p.pred_task_id)
-        extra = "M" if pact.task_type=='TT_FinMile' else "T"
-        if first.task_type == 'TT_FinMile' or True:
-            if pact.task_type != 'TT_FinMile':
-                dot.node(str(pact.task_id), f'{extra}/{pact.task_code}')
-
-            #print(f'  adding edge {p.task_id}, {p.pred_task_id}')
+        tmp = gvdb.get(p.pred_task_id,None)
+        #if tmp!=None and tmp==True: continue
+        if gv_add_node(p.pred_task_id,last, dot,xer, tot+1):
+        # pact = xer.activities.find_by_id(p.pred_task_id)
             dot.edge(str(p.task_id), str(p.pred_task_id), color='blue')
+            rc=True
 
-    for p in preds:    
-        process_node(p.pred_task_id,dot,tot+1,parent_ms)
+    if rc:
+        print(f'adding intermediate node {first.task_code}')
+        dot.node(str(first.task_id), desc)
+
+    gvdb[first.task_id] = rc
+
+    # depth of recursion allowed
+    # if tot>3: return
     #print(f'finishing node {first.task_id}')
+    return rc
 
-def do_one(first_id, first, xer):
-    # interesting attributes
-    first = xer.activities.find_by_code(first_id)
-    print(dir(xer.activities))
+def use_gv(first, last, xer):
     print(f"starting point: task_id={first.task_id}, task_code={first.task_code}, {first.task_name}, task_type={first.task_type}")
 
-    dot=gv.Digraph(comment='sched',strict=True, format='png')
-    process_node(first.task_id,dot,0)
-    process_up(first.task_id,dot,0)
-    fname=f'graph_{first_id}.gv'
+    dot=gv.Digraph(comment='sched',strict=True, format='pdf')
+    gv_add_node(first.task_id,last, dot,xer,0)
+    # gv_go_up(first.task_id,dot,xer,0)
+    fname=f'graph_{first.task_code}.gv'
     dot.render(fname).replace('\\', '/')
     dot.render(fname, view=True)
 
-def process(func):
+
+def process_gv(first_code, last_code):
     xer = Reader("../MAGIS Status with August 2022 Input.xer")
     acts = list(xer.activities)
     rels = list(xer.relations)
     typ = 'TT_FinMile'
 
-    if len(sys.argv) >1:
-        first_id=sys.argv[1]
-        ids=[first_id]
-    else:
-        print("Using list")
-        ids=['A1503560' # laser-atom interactions
-            ,'A1503530' # two atom sources installed
-            ,'A0201090' # atom source two 100% complete
-            ,'A1206110' # Milestone - Phase 2 shaft installation complete
-            ,'A1503540' # CRITICAL Milestone - First laser beam
-            ,'A1803230' # INDICATOR Milestone - First Atoms
-            ,'A1503520' # CRITICAL - Shaft Modular Sections Installed
-        ]
-        ids=['A1503780']
+    first = xer.activities.find_by_code(first_code)
+    last = xer.activities.find_by_code(last_code)
+    use_gv(first, last, xer)
 
-    for id in ids:
-        func(id, first, xer)
+alldb = set()
 
-def using_nx(id, first, xer):
-    dot=gv.Digraph(comment='sched',strict=True, format='png')
-    process_node(first.task_id,dot,0)
-    process_up(first.task_id,dot,0)
-    fname=f'graph_{first_id}.gv'
+def nx_add_node(first_id, g, xer, tot):
+    first = xer.activities.find_by_id(first_id)
+
+    if first.task_code in alldb:
+        return
+
+    extra = "M" if first.task_type=='TT_FinMile' else "T"
+    g.add_node(first.task_code, name=first.task_name, type=extra)
+    alldb.add(first.task_code)
+    preds = xer.relations.get_predecessors(first.task_id)
+
+    for p in preds:
+        # this is a bad way to do it - looking for acts by id to find code
+        # could use the task_id as the identifier for nodes and edges
+        pact = xer.activities.find_by_id(p.pred_task_id)
+        act = xer.activities.find_by_id(p.task_id)
+        extra = "M" if pact.task_type=='TT_FinMile' else "T"
+        g.add_edge(act.task_code, pact.task_code)
+
+    for p in preds:    
+        nx_add_node(p.pred_task_id,g,xer,tot+1)
+
+    print(f'done with node {first.task_code} level {tot}')
+    return True
+
+def process_nx(first_code, last_code):
+    xer = Reader("../MAGIS Status with August 2022 Input.xer")
+    acts = list(xer.activities)
+    rels = list(xer.relations)
+    typ = 'TT_FinMile'
+
+    first = xer.activities.find_by_code(first_code)
+    g=nx.DiGraph()
+    print("starting build of graph")
+    nx_add_node(first.task_id,g,xer,0)
+    print("starting find of paths")
+    ps = nx.all_simple_paths(g, first_code, last_code)
+
+    dot=gv.Digraph(comment='sched',strict=True, format='pdf')
+
+    for p in ps:
+        print(p)
+        for i in range(len(p)-1):
+            dot.edge(p[i],p[i+1], color='blue')
+
+    fname=f'graph_nx_{first.task_code}.gv'
     dot.render(fname).replace('\\', '/')
     dot.render(fname, view=True)
 
-# using graphviz directly
-process(do_one)
 
-# using networkx to find all paths from last to first
-process(using_nx)
+if __name__ == '__main__':
+
+    # some interesting codes
+    ids=['A1503560' # laser-atom interactions
+        ,'A1503530' # two atom sources installed
+        ,'A0201090' # atom source two 100% complete
+        ,'A1206110' # Milestone - Phase 2 shaft installation complete
+        ,'A1503540' # CRITICAL Milestone - First laser beam
+        ,'A1803230' # INDICATOR Milestone - First Atoms
+        ,'A1503520' # CRITICAL - Shaft Modular Sections Installed
+    ]
+
+    # other interesting starting / ending points
+    # remember that last means furtherest predecessor, or earlier one
+
+    #first_code='A1503560' # laser-atom interactions
+    #first_code='A1503570' # INDICTOR - shaft modular section installed
+    #last_code='A1004010'  # DAQ for modular assembly shipped
+    #last_code='A0101000'  # DOE project start
+    #last_code='A1503790'  # installation of 17 modular sections
+
+    if len(sys.argv) >2:
+        first_code=sys.argv[1]
+        last_code=sys.argv[2]
+    else:
+        first_code='A1503560' # laser-atom interactions
+        first_code='A1206110' # Milestone - Phase 2 shaft installation complete
+        last_code='A1004010'  # DAQ for modular assembly shipped
+        last_code='A1503500'  # CRITICAL - civil design for shaft complete
+
+    # using graphviz directly
+    #process_gv(first_code, last_code)
+
+    # using networkx to find all paths from last to first
+    process_nx(first_code, last_code)
 
 
 
