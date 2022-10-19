27eab5561eddaa327a08b2989b7c8f9e7d99eaa9
Jim Kowalkowski <jbk@fnal.gov>
2022-10-19 15:47:39 -0500

    command line options and fixed up colors and shapes

--- test.py
+++ test.py
@@ -38,23 +38,27 @@ def gv_go_up(first_id,dot, xer, tot):
 
 gvdb = {}
 
-def gv_add_node(first_id, last, dot, xer, tot):
+def gv_add_node(first_id, last, dot, xer, tot, special):
     first = xer.activities.find_by_id(first_id)
     #print(f'adding node {first.task_id}, {first.task_code} / {first.task_name}')
     rc=False
 
     if first.task_id in gvdb:
-        print(f'{first.task_id} already processed completely')
+        #print(f'{first.task_id} already processed completely')
         return gvdb[first.task_id]
 
     if first.task_type == 'TT_FinMile':
-        desc = f'{first.task_code}/{first.task_name[0:]}'
+        desc = f'{first.task_code}\n{first.task_name[0:]}'
+        col='purple'
     else:
-        desc = f'{first.task_code}'
+        desc = f'{first.task_code}\n{first.task_name[0:25]}'
+        col='black'
+
+    shape = 'rect' if first.task_code in special else 'ellipse'
 
     if first.task_id == last.task_id:
-        dot.node(str(first.task_id), desc)
-        print("hit first == last")
+        dot.node(str(first.task_id), label=desc,color=col, shape=shape)
+        #print("hit first == last")
         return True
 
     preds = xer.relations.get_predecessors(first.task_id)
@@ -66,14 +70,14 @@ def gv_add_node(first_id, last, dot, xer, tot):
     for p in preds:
         tmp = gvdb.get(p.pred_task_id,None)
         #if tmp!=None and tmp==True: continue
-        if gv_add_node(p.pred_task_id,last, dot,xer, tot+1):
+        if gv_add_node(p.pred_task_id,last, dot,xer, tot+1, special):
         # pact = xer.activities.find_by_id(p.pred_task_id)
             dot.edge(str(p.task_id), str(p.pred_task_id), color='blue')
             rc=True
 
     if rc:
-        print(f'adding intermediate node {first.task_code}')
-        dot.node(str(first.task_id), desc)
+        #print(f'adding intermediate node {first.task_code}')
+        dot.node(str(first.task_id), label=desc,color=col,shape=shape)
 
     gvdb[first.task_id] = rc
 
@@ -82,18 +86,18 @@ def gv_add_node(first_id, last, dot, xer, tot):
     #print(f'finishing node {first.task_id}')
     return rc
 
-def use_gv(first, last, xer):
+def use_gv(first, last, xer, special):
     print(f"starting point: task_id={first.task_id}, task_code={first.task_code}, {first.task_name}, task_type={first.task_type}")
 
     dot=gv.Digraph(comment='sched',strict=True, format=output_format)
-    gv_add_node(first.task_id,last, dot,xer,0)
+    gv_add_node(first.task_id,last, dot,xer,0,special)
     # gv_go_up(first.task_id,dot,xer,0)
     fname=f'gv_{first.task_code}_{last.task_code}.gv'
     dot.render(fname).replace('\\', '/')
     dot.render(fname, view=True)
 
 
-def process_gv(first_code, last_code):
+def process_gv(first_code, last_code, special):
     xer = Reader("../MAGIS Status with August 2022 Input.xer")
     acts = list(xer.activities)
     rels = list(xer.relations)
@@ -114,7 +118,7 @@ def process_gv(first_code, last_code):
     # print(f'{first.task_code} {stat_code} {st_date} {en_date} {till_start} {till_end}')
     # sys.exit(0)
 
-    use_gv(first, last, xer)
+    use_gv(first, last, xer, special)
 
 alldb = set()
 
@@ -252,17 +256,19 @@ def process_edges(g,root,leaf):
     if len(ps)==0: # at latest task in tree
         g.nodes[root]['LF'] = EF
         LS=EF-dur
+        LF=EF
         g.nodes[root]['LS'] = LS
         return LS
 
     if True:
         prev_LSs=[process_edges(g,p,leaf) for p in ps]
-        LS = min(prev_LSs)
-        g.nodes[root]['LF']=LS
-        g.nodes[root]['LS']=LS-dur
+        LF = min(prev_LSs)
+        LS=LF-dur
+        g.nodes[root]['LF']=LF
+        g.nodes[root]['LS']=LS
 
     #print(f'done with successors of leaf {leaf} for root {root}')
-    return LS-dur
+    return LS
 
 # don't do this
 def nx_mark_crit(g,root,leaf):
@@ -335,6 +341,23 @@ if __name__ == '__main__':
         ,'A1503520' # CRITICAL - Shaft Modular Sections Installed
     ]
 
+    inter={
+        '1':['A1004010','A1503500','A1503240','A1503770','A1206100','A1503790','A1503570','A1503520','A1206110']
+        ,'2':['A0201090','A0214020','A1503570','A1503530']
+        ,'3':['A0307030','A0301050','A1503760','A1503330','A1802570','A0309020','A1802380','A1503220','A1004030','A1503590','A1503540']
+        ,'4':['A0501050','A0201090','A0607010','A1203180','A0508020','A1802360','A1004020','A0214020','A1802370','A1803230','A1803240']
+        ,'5':['A1503280','A1802240','A1803330','A1802330','A1802340','A1004040','A1503560','A1503600']
+        ,'none':[]
+    }
+    inter_names={
+        '5':'Laser-atom interactions',
+        '4':'First atoms',
+        '3':'First laser beam',
+        '2':'Two atom sources installed',
+        '1':'Shaft modular sections installed'
+    }
+
+
     # other interesting starting / ending points
     # remember that last means furtherest predecessor, or earlier one
     # root = first (earliest), leaf = last (latest)
@@ -347,22 +370,43 @@ if __name__ == '__main__':
     #last_code='A0101000'  # DOE project start
     #last_code='A1503790'  # installation of 17 modular sections
 
-    if len(sys.argv) >2:
-        first_code=sys.argv[1]
-        last_code=sys.argv[2]
+    if len(sys.argv)==2 and (sys.argv[1]=='help' or sys.argv[1]=='--help'):
+        print("usage: <key from below> <latest task code> <earliest task code>")
+        for k,v in inter_names.items():
+            print(f'{k}\t{v}')
+        sys.exit(0)    
+
+    if len(sys.argv)>1:
+        special=sys.argv[1]
     else:
-        first_code='A1503560' # laser-atom interactions
-        first_code='A1206110' # Milestone - Phase 2 shaft installation complete
-        #first_code='A1004010'  # DAQ for modular assembly shipped
-        first_code='A1503240' # construction bid
-        last_code='A1004010'  # DAQ for modular assembly shipped
-        last_code='A1503500'  # CRITICAL - civil design for shaft complete
-        last_code='A0101000'  # DOE project start
-        last_code='A0100000'  # start?
+        special=['1']
+
+    print(f'using list named {inter_names[special]}')
+    special = inter[special]
+
+    # default codes to use
+    first_code = special[-1]
+    last_code = special[0]
+
+    if len(sys.argv) >3:
+        first_code=sys.argv[2]
+        last_code=sys.argv[3]
+
+    # more examples
+    #first_code='A1503560' # laser-atom interactions
+    #first_code='A1206110' # Milestone - Phase 2 shaft installation complete
+    #first_code='A1004010'  # DAQ for modular assembly shipped
+    #first_code='A1503240' # construction bid
+    #last_code='A1004010'  # DAQ for modular assembly shipped
+    #last_code='A1503500'  # CRITICAL - civil design for shaft complete
+    #last_code='A0101000'  # DOE project start
+    #last_code='A0100000'  # start?
+
 
     # using graphviz directly
-    process_gv(first_code, last_code)
+    process_gv(first_code, last_code,special)
 
+    sys.exit(0)
     # using networkx to find all paths from last to first
     g,root,leaf,ps=process_nx(first_code, last_code)
     process_longest_dur(g,root[0],leaf[0])
